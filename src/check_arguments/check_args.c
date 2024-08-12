ae56bec0befc646f5502e7a2822fddd9ad3957d1
Aurelien Focant <24444885+AurelienFocant@users.noreply.github.com>
2024-08-12 11:31:24 +0100

    added function to check file extension

--- src/check_arguments/check_args.c
+++ src/check_arguments/check_args.c
@@ -2,7 +2,13 @@
 
 void	fn_check_map_file(char *map_file)
 {
-	(void) map_file;
+	char	*ptr;
+
+	ptr = ft_strchr(map_file, '.');
+	if (!ptr)
+		fn_error_exit("Wrong map extension", ENOEXEC, 3);
+	if (ft_strncmp(++ptr, "ber", 4) != 0)
+		fn_error_exit("Wrong map extension", ENOEXEC, 4);
 }
 	
 void	fn_check_nb_args(int argc)
@@ -14,8 +20,5 @@ void	fn_check_nb_args(int argc)
 void	fn_check_args(int argc, char **argv)
 {
 	fn_check_nb_args(argc);	
-	(void) argv;
-	/*
 	fn_check_map_file(argv[1]);
-	*/
 }
